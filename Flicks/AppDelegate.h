5f3ca6df81e77c32d66e13c17cd23890f77934d0
Michael Cieplak <mcieplak@yahoo-inc.com>
2017-01-23 19:47:18 -0800

    Initial Commit

---
+++ Flicks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flicks
+//
+//  Created by Michael Cieplak on 1/23/17.
+//  Copyright © 2017 Michael Cieplak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
